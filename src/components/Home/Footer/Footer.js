f386946ca08dbd257ec8172a8dbb61dac7f4c0d0
Sujon Ali <sujon.cse.1803100@gmail.com>
2021-11-21 15:59:58 +0600

    Edition

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,6 +1,11 @@
+
 import React from 'react';
 import './Footer.css';
 import FooterData from './FooterData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
 
 const footertData = [
     {
@@ -32,8 +37,16 @@ const Footer = () => {
                     <div className="col-md-3 col-sm-6 p-3">
                         <h5 style={{ color: '#1CC7C1' }}>Our Address</h5>
                         <p>New Yorks-101010 Hudhon Yards</p>
-                        <p>Call now..</p>
-                        <button className="btn btn-primary">+15980235128</button>
+                        <div className="d-flex mb-3">
+                            <a className="me-3" href="https://web.facebook.com/sujonali8276/" target="_blank"><FontAwesomeIcon style={{ height: '50px', width: '50px', color: 'blue' }} icon={faFacebook} /></a>
+                            <a className="me-3" href="https://web.facebook.com/sujonali8276/" target="_blank"><FontAwesomeIcon style={{ height: '50px', width: '50px', color: 'red' }} icon={faYoutube} /></a>
+                            <a className="me-3" href="https://web.facebook.com/sujonali8276/" target="_blank"><FontAwesomeIcon style={{ height: '50px', width: '50px', color: 'green' }} icon={faLinkedin} /></a>
+                        </div>
+                        <p>Call now</p>
+                        <button className="btn btn-success rouned-pill">
+                            <FontAwesomeIcon style={{ height: '20px', marginRight: '5px', color: 'white' }} icon={faPhone} />
+                            +8801782562576
+                        </button>
                     </div>
                 </div>
                 <b><p className="text-center">Copyright 2021-All Rights Reserved</p></b>
